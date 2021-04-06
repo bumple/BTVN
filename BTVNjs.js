8e4ad073c6e21a154c37d36880edc97094b2c821
bumple <81680183+bumple@users.noreply.github.com>
2021-04-06 22:16:53 +0700

    sua bai1

--- BTVNjs.js
+++ BTVNjs.js
@@ -7,4 +7,4 @@ if (b<18.5){ alert("Bạn đang bị gầy");
             }else if(b>=25 && b<30){alert("Bạn đang bị mập");
               }else if(b>=30 && b<35){alert("Bạn đang bị béo C1");
                  }else if(b>=35 && b<40){alert("Bạn đang bị béo C2");
-                     }else if("Bạn đang bị nguy hiểm");
+                     }else {alert("Bạn đang bị nguy hiểm");}
